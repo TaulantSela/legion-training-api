b542c4e64352ccc8e1ac117b3cd22f6f0ff9dfb0
Taulant Sela <taulant.sela@hoyo.tech>
2022-10-21 23:30:55 +0200

    add simple crud operations for workouts

---
+++ src/services/workoutService.js
@@ -0,0 +1,47 @@
+/* 
+***
+    Business Logic goes here !
+***
+*/
+
+const { v4: uuid } = require("uuid");
+const Workout = require("../database/Workout");
+
+function getAllWorkouts() {
+  const allWorkouts = Workout.getAllWorkouts(); // no transofrmation needed because the file is already JSON
+  return allWorkouts;
+}
+
+const getOneWorkout = (workoutId) => {
+  const workout = Workout.getOneWorkout(workoutId);
+  return workout;
+};
+
+const createNewWorkout = (newWorkout) => {
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuid(),
+    createdAt: new Date().toLocaleString("en-US", { timezone: "UTC" }),
+    updatedAt: new Date().toLocaleString("en-US", { timezone: "UTC" }),
+  };
+
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+  return createdWorkout;
+};
+
+const updateOneWorkout = (workoutId, changes) => {
+  const updatedWorkout = Workout.updateOneWorkout(workoutId, changes);
+  return updatedWorkout;
+};
+
+const deleteOneWorkout = (workoutId) => {
+  Workout.deleteOneWorkout(workoutId);
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};

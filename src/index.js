fa92ba36bef9ac40d7fb954fb893d7bdc64ef59b
Taulant Sela <taulant.sela@hoyo.tech>
2022-10-25 14:14:34 +0200

    add swagger docs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const apicache = require("apicache");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
 const v1MemberRouter = require("./v1/routes/memberRoutes");
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
 //  data cache => redis / apicache
 
@@ -17,6 +18,7 @@ app.use("/api/v1/members", v1MemberRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
 });
 
 /* 
